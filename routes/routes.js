5a90c8253a4ff9d0a2a49ff000a7ee9838fdaf4c
gwuah <griffith@dolphtech.com>
2018-07-01 13:10:38 +0000

    added create routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
+const feedback = require("../controllers/feedback");
 const express = require("express");
 const router = express.Router();
 
+router.post('/create', feedback.createFeedback)
 
 router.all('*', function(req, res) {
   res.send("Error 404");
