28b6e7e5d610cad5f0fb4c95382e7179b9d7a8e7
gwuah <griffith@dolphtech.com>
2018-07-01 16:09:49 +0000

    integrated socket.io

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const app = require("./app");
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 if (process.env.MODE == "development") {
+  console.log(process.env.DEVELOPMENT_DB_URI)
 	mongoose.connect(process.env.DEVELOPMENT_DB_URI);
 } else if (process.env.MODE == "production") {
 	mongoose.connect(process.env.PRODUCTION_DB_URI)
@@ -15,6 +19,6 @@ mongoose.connection.on('error', async (err) => {
   process.exit();
 });
 
-const server = app.listen(process.env.PORT, () => {
+const server = http.listen(process.env.PORT, () => {
 	console.log(`Main server running → port ${process.env.PORT}`)
 });
