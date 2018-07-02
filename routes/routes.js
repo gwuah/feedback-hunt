20c5ce9dbdc1630e7754132c81a7a9cd7f83d453
gwuah <griffith@dolphtech.com>
2018-07-02 11:12:46 +0000

    grouped routes and added few extra routes for login signup

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
 const feedback = require("../controllers/feedbackController");
+const user = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', feedback.getAllFeedbacks);
-router.post('/create', feedback.createFeedback);
+/* client controllers */
+router.get('/', feedback.home);
+router.get('/user', feedback.user);
+
+/* user */
+router.post('/create/:id', feedback.createFeedback);
+
+/* merchant controllers */
+router.post('/signup', user.signup)
+router.post('/login', user.login)
+router.get('/feedback/:ownerId', feedback.getFeedbackOfUser);
+
+
 
 router.all('*', function(req, res) {
   res.send("Error 404");
